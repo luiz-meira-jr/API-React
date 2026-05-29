2041dacaa4b33bd96bb47f70c1ba71bfe7242b6d
Luiz Carlos Meira Jr <87606711+luiz-meira-jr@users.noreply.github.com>
2026-05-29 08:36:02 -0300

    Envelopando itens no card
    Envelopando cada produto (card) com um Link que direciona para a página de detalhes do produto, usando rotas

--- src/components/ProdutosCard.js
+++ src/components/ProdutosCard.js
@@ -1,14 +1,17 @@
 import '../components/css/produtos.css';
+import { Link } from 'react-router-dom';
 
 function ProdutosCard(props){
 
     return(
-        <div className="card">
-            <img src={props.produto.image}/>
-            <h4>{props.produto.title}</h4>
-            <p className='preco'>R$ {props.produto.price.toFixed(2)}</p>
-            <p className='categoria'><strong>Categoria: </strong>{props.produto.category}</p>
-        </div>
+        <Link className='pgprod' to={`/produtos/${props.produto.id}`}>
+            <div className="card">
+                <img src={props.produto.image}/>
+                <h4>{props.produto.title}</h4>
+                <p className='preco'>R$ {props.produto.price.toFixed(2)}</p>
+                <p className='categoria'><strong>Categoria: </strong>{props.produto.category}</p>
+            </div>
+        </Link>
     )
 }
 
